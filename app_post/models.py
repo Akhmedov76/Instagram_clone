85c5622c3d698369ce758307a8a84d146ca29e04
Jasur <ahmedovj5016@gmail.com>
2024-11-14 18:08:18 +0500

    create post model for posts

--- app_post/models.py
+++ app_post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    caption = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Foydalanuvchi kim post qoldirganini bilish
+
+    def __str__(self):
+        return self.title
+
+
+
