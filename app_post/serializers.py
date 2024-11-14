5cf3c19271166db99554d3b8fe48663b2d6a8005
Jasur <ahmedovj5016@gmail.com>
2024-11-14 18:21:06 +0500

    create serializers

--- app_post/serializers.py
+++ app_post/serializers.py
@@ -1,5 +1,41 @@
 from rest_framework import serializers
+
+from app_post.models import *
+
+
 class CommentSerializers(serializers.ModelSerializer):
     class Meta:
-        model = Co
-        fields = '__all__'
+        model = CommentModel
+        fields = ['user', 'text', 'created_at']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
+
+class LikeSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = LikeModel
+        fields = ['user']
+
+    def validate(self, data):
+        if data['user'] == self.context['request'].user:
+            raise serializers.ValidationError('You can not like your own comment')
+        return data
+
+
+class PostSerializers(serializers.ModelSerializer):
+    comments = CommentSerializers(many=True, read_only=True)
+    likes_count = serializers.IntegerField(read_only=True)
+    is_liked = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = PostModel
+        fields = ['id', 'title', 'image', 'caption', 'created_at', 'updated_at', 'user', 'comments']
+        read_only_fields = ['created_at', 'updated_at', 'user']
+
+
+class PostCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostModel
+        fields = ['title', 'image', 'caption']
