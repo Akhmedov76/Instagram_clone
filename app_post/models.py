bcc41b52e9756c631352400d41f0c7c0c9ff56c1
Jasur <ahmedovj5016@gmail.com>
2024-11-14 18:09:52 +0500

    create like model for posts

--- app_post/models.py
+++ app_post/models.py
@@ -25,4 +25,9 @@ class Comment(models.Model):
         return f"Comment by {self.user.username} on {self.post.title}"
 
 
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Like by {self.user.username} on {self.post.title}"
