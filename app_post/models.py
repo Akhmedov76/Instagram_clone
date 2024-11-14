7703e3ccca68e142d65d99156ef81a4ce9ff6a13
Jasur <ahmedovj5016@gmail.com>
2024-11-14 18:08:38 +0500

    create comment model for posts

--- app_post/models.py
+++ app_post/models.py
@@ -15,4 +15,14 @@ class Post(models.Model):
         return self.title
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
+
+
 
