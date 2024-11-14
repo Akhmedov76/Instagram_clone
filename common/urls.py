c7f5460306e84296d7c502e83341cb05ee73e824
Jasur <ahmedovj5016@gmail.com>
2024-11-14 15:48:01 +0500

    create login view

--- common/urls.py
+++ common/urls.py
@@ -1,3 +1,9 @@
-urlpatterns= [
+from django.urls import path
 
+from app_accounts import views
+
+urlpatterns = [
+    path('register/', views.RegistrationView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('verify-email/', views.VerificationView.as_view(), name='verify_email'),
 ]
