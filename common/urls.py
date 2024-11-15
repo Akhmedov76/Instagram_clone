6df9e882659fa1a4e67e265d6c2835dc3e260c6d
Jasurbek <163736163+Akhmedov76@users.noreply.github.com>
2024-11-15 09:49:32 +0500

    Update urls.py

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from app_accounts import views
+from app_accounts import *
 from app_post.views import *
 
 router = DefaultRouter()
@@ -9,9 +9,9 @@ router.register('post', post_views.PostView, basename='posts')
 router.register('comment', post_views.CommentView, basename='comments')
 
 urlpatterns = [
-    path('register/', views.RegistrationView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('verify-email/', views.VerificationView.as_view(), name='verify_email'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('verify-email/', VerificationView.as_view(), name='verify_email'),
     path('create/', PostCreateView.as_view(), name='post-create'),
     path('comment/<int:post_id>/', CommentCreateAPIView.as_view(), name='comment-create'),
     path('like/<int:post_id>/', LikeCreateAPIView.as_view(), name='like-create'),
